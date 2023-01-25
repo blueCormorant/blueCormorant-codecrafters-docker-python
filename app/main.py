6eaa7ce8076a396276071e9f4bc05dba8dd49ce0
blueCormorant <lucavdelsignore@gmail.com>
2023-01-25 14:52:39 -0500

    Stage 2

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
 import subprocess
 import sys
 
-
 def main():
 
     command = sys.argv[3]
     args = sys.argv[4:]
-    
+    if args[0] == "echo":
+        cmd = "echo"
+        output_stream = sys.stdout
+    elif args[0] == "echo_stderr":
+        mcd = "echo"
+        output_stream = sys.stderr
+    else:
+        raise Exception("Command not recognized")    
+
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    print(completed_process.stdout.decode("utf-8"), file=output_stream)
 
 if __name__ == "__main__":
     main()
