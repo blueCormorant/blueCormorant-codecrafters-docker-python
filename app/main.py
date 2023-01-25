3363e0594659739f4cb113eec62a6696f3783cd2
blueCormorant <lucavdelsignore@gmail.com>
2023-01-25 15:27:57 -0500

    Stage 3

--- app/main.py
+++ app/main.py
@@ -7,13 +7,19 @@ def main():
     args = sys.argv[4:]
     
     completed_process = subprocess.run([command, *args], capture_output=True)
-    
-    if args[0] == "echo":
-        print(completed_process.stdout.decode("utf-8").strip(), file=sys.stdout)
-    elif args[0] == "echo_stderr":
-        print(completed_process.stderr.decode("utf-8").strip(), file=sys.stderr)
+
+    if completed_process.returncode == 0:
+        if args[0] == "echo":
+            print(completed_process.stdout.decode("utf-8").strip(), file=sys.stdout)
+        elif args[0] == "echo_stderr":
+            print(completed_process.stderr.decode("utf-8").strip(), file=sys.stderr)
+        elif args[0] == "exit":
+            sys.exit(0)
+        else:
+            raise Exception("Command not recognized")    
     else:
-        raise Exception("Command not recognized")    
+        sys.exit(completed_process.returncode)
+
 
 if __name__ == "__main__":
     main()
