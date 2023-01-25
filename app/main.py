9b20d7066654f9e7857e6e58c3dd1b917c60d610
blueCormorant <lucavdelsignore@gmail.com>
2023-01-25 15:16:01 -0500

    Fix bug

--- app/main.py
+++ app/main.py
@@ -5,17 +5,15 @@ def main():
 
     command = sys.argv[3]
     args = sys.argv[4:]
+    
+    completed_process = subprocess.run([command, *args], capture_output=True)
+    
     if args[0] == "echo":
-        cmd = "echo"
-        output_stream = sys.stdout
+        print(completed_process.stdout.decode("utf-8").strip(), file=sys.stdout)
     elif args[0] == "echo_stderr":
-        cmd = "echo"
-        output_stream = sys.stderr
+        print(completed_process.stderr.decode("utf-8").strip(), file=sys.stderr)
     else:
         raise Exception("Command not recognized")    
-
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8").strip(), file=output_stream)
 
 if __name__ == "__main__":
     main()
